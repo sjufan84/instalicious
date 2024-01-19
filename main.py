9158084ab168c7cd97d4930158c4af179287ef7a
David Thomas <dave_thomas@enoughwebapp.com>
2024-01-19 14:31:13 -0600

    added in file uploader

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 import streamlit as st
 import asyncio
 from utils.image_utils import generate_image
-from utils.post_utils import create_recipe_post, create_description_post
+from utils.post_utils import create_post, alter_image
 import logging
+import base64
+
+# Function to encode the image
+async def encode_image(image_file):
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -13,10 +18,10 @@ def init_session_variables():
     # Initialize session state variables
     session_vars = [
         "current_post", "current_hashtags", "current_image_prompt", "current_image", "post_page",
-        "current_model", "model_selection"
+        "current_model", "model_selection", "generate_image", "image_choice"
     ]
     default_values = [
-        None, None, None, None, "post_home", "gpt-3.5-turbo-1106", "GPT-3.5"
+        None, None, None, None, "post_home", "gpt-3.5-turbo-1106", "GPT-3.5", False, False
     ]
 
     for var, default_value in zip(session_vars, default_values):
@@ -36,19 +41,25 @@ async def post_home():
     logger.debug("Entering post_home function")
     st.markdown("""##### I would like to generate a post from:""")
 
-    post_type = st.selectbox(
-        "Post Type",
-        ("Existing Recipe", "Description of a recipe or restaurant experience")
-    )
-    logger.debug(f"Post type selected: {post_type}")
+    image = None
 
-    if post_type == "Existing Recipe":
-        post_input = st.text_area("Recipe Input", value="Paste your recipe here")
-    else:
-        post_input = st.text_area(
-            "Description Input", value="Describe your recipe, dish, or restaurant experience here"
+    post_prompt = st.text_area("""Describe the post you would like to generate.
+    This could be a recipe, a description of a dish, a description of a restaurant experience, etc.
+    You can also paste a recipe here if you would like to generate a post from an existing recipe.""")
+
+    st.session_state.generate_image = st.checkbox("""Would you like to generate an image as well?
+    This could be solely based on the prompt, or you can upload an existing image to be enhanced.""")
+
+    if st.session_state.generate_image:
+        st.session_state.image_choice = st.selectbox(
+            "Image Choice", ("Based on the prompt", "Use an existing image")
         )
-    logger.debug(f"Post input: {post_input}")
+        if st.session_state.image_choice == "Use an existing image":
+            # Show a file upoloader that only accepts image files
+            image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+        # Convert the image to a base64 string
+        if image:
+            image_url = await encode_image(image)
 
     st.radio(
         ":rainbow[AI Model Selection]", options=["GPT-3.5", "GPT-4"], horizontal=True, index=None,
@@ -60,26 +71,39 @@ async def post_home():
     logger.debug(f"Generate post button pressed: {generate_post_button}")
 
     if generate_post_button:
-        if post_type == "Existing Recipe":
-            with st.spinner("Generating your post. This may take a minutes or two,\
+        if st.session_state.image_choice == "Based on the prompt" and st.session_state.generate_image:
+            with st.spinner("Generating your post. This may take a minute or two,\
             but you can't rush greatness..."):
-                logger.debug("Generating post from recipe...")
-                post_response = await create_recipe_post(post_input)
+                logger.debug("Generating post...")
+                post_response = await create_post(post_type="with_image", prompt=post_prompt)
                 if post_response:
                     st.session_state["current_post"] = post_response["post"]
                     st.session_state["current_hashtags"] = post_response["hashtags"]
                     st.session_state["current_image_prompt"] = post_response["image_prompt"]
                     st.session_state.post_page = "display_post"
                     st.rerun()
-        else:
-            with st.spinner("Generating your post. This may take a minutes or two,\
+        elif st.session_state.image_choice == "Use an existing image" and st.session_state.generate_image:
+            with st.spinner("Generating your post. This may take a minute or two,\
             but you can't rush greatness..."):
-                logger.debug("Generating post from description...")
-                post_response = await create_description_post(post_input)
+                logger.debug("Generating post from description and image...")
+                post_response = await create_post(
+                    post_type = "no_image", prompt = post_prompt
+                )
+                image_prompt = await alter_image(
+                    prompt = post_prompt, image_url = image_url
+                )
+                st.session_state.current_post = post_response["post"]
+                st.session_state.current_hashtags = post_response["hashtags"]
+                st.session_state.current_image_prompt = image_prompt
+                st.session_state.post_page = "display_post"
+                st.rerun()
+        else:
+            with st.spinner("""Generating your post... this may take a minute,
+            but you can't rush greatness..."""):
+                post_response = await create_post(post_type="no_image", prompt=post_prompt)
                 if post_response:
                     st.session_state["current_post"] = post_response["post"]
                     st.session_state["current_hashtags"] = post_response["hashtags"]
-                    st.session_state["current_image_prompt"] = post_response["image_prompt"]
                     st.session_state.post_page = "display_post"
                     st.rerun()
 
@@ -97,7 +121,7 @@ async def display_post():
 
     with col2:
         st.markdown("""##### Image:""")
-        if not st.session_state.current_image:
+        if not st.session_state.current_image and st.session_state.current_image_prompt:
             with st.spinner("Generating your image..."):
                 st.session_state.current_image = await generate_image(
                     st.session_state["current_image_prompt"]
@@ -111,6 +135,8 @@ async def display_post():
         st.session_state.current_post = None
         st.session_state.current_hashtags = None
         st.session_state.current_image_prompt = None
+        st.session_state.generate_image = False
+        st.session_state.image_choice = False
         st.session_state.current_image = None
         st.session_state.post_page = "post_home"
         st.session_state.model_selection = "GPT-3.5"
--- utils/post_utils.py
+++ utils/post_utils.py
@@ -22,6 +22,16 @@ logger.debug(f"Current model: {st.session_state.current_model}")
 # core_models = ["gpt-4-1106-preview", "gpt-3.5-turbo-1106"]
 # logger.debug(f"Core models: {core_models}")
 
+class ImagePostResponse(BaseModel):
+    """ Image Post Response Model """
+    post: str = Field(..., title="post", description="The generated post text.")
+    hashtags: str = Field(..., title="hashtags", description="The generated hashtags.")
+    image_prompt: str = Field(..., title="image_prompt", description="The prompt used to generate the image.")
+
+class TextPostResponse(BaseModel):
+    """ Text Post Response Model """
+    post: str = Field(..., title="post", description="The generated post text.")
+    hashtags: str = Field(..., title="hashtags", description="The generated hashtags.")
 
 initial_message = [
     {
@@ -33,128 +43,140 @@ initial_message = [
     }
 ]
 
-max_retries = 3
 
-# Log the initial message
-logger.debug(f"Initial message: {initial_message}")
+async def get_messages(post_option: str, prompt: str):
+    """ Get the messages for the post option """
+    messages = []
+    if post_option == "with_image":
+        messages = [
+            {
+                "role": "system",
+                "content": f"""The user would like for you to generate an Instagram post optimized
+                for engagement and virality based on the prompt {prompt} they have given.  This could
+                be a recipe, a description of a dish, a description of a restaurant experience, etc.
+                Your response should be returned as a JSON object in the following format:
 
-class RecipePostResponse(BaseModel):
-    """ Recipe Post Response Model """
-    post: str = Field(..., title="Post", description="The generated post.")
-    hashtags: str = Field(..., title="Hashtags", description="The generated hashtags.")
-    image_prompt: str = Field(..., title="Image Prompt", description="The prompt used to generate the image.")
+                post: str = The post to be generated.
+                hashtags: List[str] = The hashtags to be generated.
+                image_prompt: str = An image prompt to send to dall-e for generation based on the user prompt.
+                Should be photo-realistic and life-like as possible.
 
-async def create_recipe_post(recipe) -> RecipePostResponse:
-    """ Generate a post based on a recipe """
-    logger.debug(f"Creating recipe post for: {recipe}")
+                Ensure that the post is presented in a clear and organized manner
+                and that it adheres to the {ImagePostResponse} schema as outlined above."""
+            },
+            {
+                "role" : "system", "content" : """Example Response:\n
+                {
+                    "post": "This is the post that was generated.",
+                    "hashtags": ["hashtag1", "hashtag2", "hashtag3"],
+                    "image_prompt": "This is the image prompt that was generated."
+                }"""
+            }
+        ]
+
+        total_messages = initial_message + messages
+
+    elif post_option == "no_image":
+        messages = [
+            {
+                "role": "system", "content": f"""The user would like
+                for you to generate an Instagram post optimized
+                for engagement and virality based on the prompt {prompt} they have given.  This could
+                be a recipe, a description of a dish, a description of a restaurant experience, etc.
+                Your response should be returned as a JSON object in the following format:
+
+                post: str = The post to be generated.
+                hashtags: List[str] = The hashtags to be generated.
+
+                Ensure that the post is presented in a clear and organized manner
+                and that it adheres to the {TextPostResponse} schema as outlined above."""
+            },
+            {
+                "role": "system", "content": """Example Response:\n
+                {
+                    "post": "This is the post text that was generated.",
+                    "hashtags": ["hashtag1", "hashtag2", "hashtag3"]
+                }"""
+            }
+        ]
+
+        total_messages = initial_message + messages
+
+    logger.debug(f"Total messages: {total_messages}")
+
+    return total_messages
+
+
+async def create_post(post_type: str, prompt: str):
+    """ Generate a post based on a user prompt"""
+    messages = await get_messages(post_type, prompt)
+    i = 0
+    max_retries = 3
+    while i < max_retries:
+        try:
+            logger.debug(f"Trying model: {st.session_state.current_model}")
+            response = client.chat.completions.create(
+                model=st.session_state.current_model,
+                messages=messages,
+                temperature=0.75,
+                top_p=1,
+                max_tokens=750,
+                response_format={"type": "json_object"}
+            )
+            post_response = json.loads(response.choices[0].message.content)
+            logger.debug(f"Response: {post_response}")
+            logger.debug(f"Response type: {type(post_response)}")
+            return {
+                "post": post_response["post"], "hashtags": post_response["hashtags"],
+                "image_prompt": post_response["image_prompt"] if "image_prompt" in post_response else None
+            }
+
+        except OpenAIError as e:
+            logger.error(f"Error with model: {st.session_state.current_model}. Error: {e}")
+            i += 1
+
+    logger.warning("All models failed. Returning None.")
+    return None
+
+async def alter_image(prompt: str, image_url: str):
+    """ Generate a new dall-e prompt based on the user prompt and the image """
     messages = [
         {
-            "role": "system",
-            "content": f"""The user has a recipe {recipe} that they would like to generate an
-            Instagram post for.  Generate a post, hashtags, and an image prompt for Dall-e optimized
-            to generate the most viral post possible.  The post should be returned as a JSON object
-            in the following format:
-
-            post: str = The post to be generated.
-            hashtags: List[str] = The hashtags to be generated.
-            image_prompt: str = The image prompt to be generated.  Should be photo-realistic
-            and should be a picture of the dish that the recipe is for.
-
-            Ensure that the post is presented in a clear and organized manner
-            and that it adheres to the {RecipePostResponse} model as outlined above."""
+            "role": "system", "content": [
+                {
+                    "type" : "text", "text" : f"""The user has uploaded an
+                    image and a prompt {prompt} that they
+                    would like to convert into a viral Instagram post. The prompt may be a recipe, a dish,
+                    a description of a restaurant experience, etc.  Based on the prompt and the image,
+                    create an image prompt for dall-e that takes the original image and
+                    optimizes it for maxiumum engagement on Instagram.  You only need to
+                    focus on the new image prompt for
+                    dall-e.  Only return the prompt for dall-e to use to generate the new, optimized image."""
+                }
+            ]
         },
         {
-            "role" : "system", "content" : """Example Response:\n
-            {
-                "post": "This is the post that was generated.",
-                "hashtags": ["hashtag1", "hashtag2", "hashtag3"],
-                "image_prompt": "This is the image prompt that was generated."
-            }"""
+            "role" : "system", "content" : [
+                {
+                    "type" : "text", "text" : "This is the image that was passed to you:"
+                },
+                {
+                    "type" : "image_url", "image_url" : f"data:image/jpeg;base64,{image_url}"
+                }
+            ]
         }
     ]
 
-    # models = core_models
-
-    messages = initial_message + messages
-
-    i = 0
-    while i < max_retries:
-        try:
-            logger.debug(f"Trying model: {st.session_state.current_model}")
-            response = client.chat.completions.create(
-                model=st.session_state.current_model,
-                messages=messages,
-                temperature=0.75,
-                top_p=1,
-                max_tokens=750,
-                response_format={"type": "json_object"}
-            )
-            post_response = json.loads(response.choices[0].message.content)
-            logger.debug(f"Response: {post_response}")
-            logger.debug(f"Response type: {type(post_response)}")
-            return {
-                "post": post_response["post"], "hashtags": post_response["hashtags"],
-                "image_prompt": post_response["image_prompt"]
-            }
-
-        except OpenAIError as e:
-            logger.error(f"Error with model: {st.session_state.current_model}. Error: {e}")
-            i += 1
-
-    logger.warning("All models failed. Returning None.")
-    return None
-
-async def create_description_post(description: str):
-    """ Generate a post based on either a description of a dish or an experience at a restaurant """
-    logger.debug(f"Creating description post for: {description}")
-
-    messages = [
-        {
-            "role": "system",
-            "content": f"""The user has a description {description} that they would like to generate an
-            Instagram post for.  This will either be a description of a dish or an experience that they had
-            at a restaurant or both.  Generate a post, hashtags, and an image prompt for Dall-e optimized
-            to generate the most viral post possible.  The post should be returned as a JSON object
-            in the following format:
-
-            Post (post): str The post to be generated.
-            Hashtags (hashtags): List[str] The hashtags to be generated.
-            Image Prompt (image_prompt): str The image prompt to be generated.
-
-            Ensure that the post is presented in a clear and organized manner
-            and that it adheres to the {RecipePostResponse} model as outlined above and that
-            all the keys are lowercase.  The keys are ["post", "hashtags", "image_prompt"].]"""
-
-        }
-    ]
-
-    # models = core_models
-
-    messages = initial_message + messages
-
-    i = 0
-    while i < max_retries:
-        try:
-            logger.debug(f"Trying model: {st.session_state.current_model}")
-            response = client.chat.completions.create(
-                model=st.session_state.current_model,
-                messages=messages,
-                temperature=0.75,
-                top_p=1,
-                max_tokens=750,
-                response_format={"type": "json_object"}
-            )
-            post_response = json.loads(response.choices[0].message.content)
-            logger.debug(f"Response: {post_response}")
-            logger.debug(f"Response type: {type(post_response)}")
-            return {
-                "post": post_response["post"], "hashtags": post_response["hashtags"],
-                "image_prompt": post_response["image_prompt"]
-            }
-
-        except OpenAIError as e:
-            logger.error(f"Error with model: {st.session_state.current_model}. Error: {e}")
-            i += 1
-
-    logger.warning("All models failed. Returning None.")
-    return None
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4-vision-preview",
+            messages=messages,
+            max_tokens=250,
+        )
+        logger.debug(f"Response: {response}")
+        prompt_response = response.choices[0].message.content
+        logger.debug(f"Prompt response: {prompt_response}")
+        return prompt_response
+    except OpenAIError as e:
+        logger.error(f"Error generating prompt for image alteration: {e}")
+        return None
