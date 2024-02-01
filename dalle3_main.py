fa86151ca15d21818c908df1571303f2840a236a
David Thomas <dave_thomas@enoughwebapp.com>
2024-01-31 18:26:27 -0600

    added more heic support, UI edits

--- dalle3_main.py
+++ dalle3_main.py
@@ -122,12 +122,6 @@ async def post_home():
                     text-align: center;
                     font-family: 'Anton', sans-serif;
                 }
-                p {
-                    color: #000000;
-                    font-size: 1em;
-                    text-align: center;
-                    font-family: 'Arapey', serif;
-                }
         """,
     ):
         # Create a centered header using HTML with bold font
@@ -136,11 +130,10 @@ async def post_home():
         )
         # Create a centered subheader using HTML with bold font
         st.markdown(
-            """<p>
+            """<p style='text-align:center; font-family:"Arapey";'>
             Transform any meal into a stunning,
             Insta-worthy post... instantly.</p>""", unsafe_allow_html=True
         )
-    st.text("")
     st.text("")
 
     with stylable_container(
@@ -149,9 +142,22 @@ async def post_home():
         {
             font-family: 'Arapey', serif;
             font-size: 1em;
+            border-color: #000000;
+        }
+        p {
+            font-size: 1.1em;
+            text-align: start;
         }
         """,
     ):
+        st.markdown(
+            """**How it works:** Instalicio.us uses cutting-edge AI to generate stunning
+            images and social media posts optimized for engagement and virality based on your prompt.
+            This could be a description of the dish, a mood, a pasted recipe,
+            a restaurant experience you had, etc.\n\n Let your imagination run wild!"""
+        )
+        st.text("")
+
         picture_mode = st.selectbox(
             '###### 📸 Snap a Pic, 📤 Upload an Image, or Let Us Generate One For You!',
             ("Snap a pic", "Upload an image", "Let Us Generate One For You"), index=None,
@@ -216,44 +222,19 @@ async def post_home():
 
     st.text("")
     st.text("")
-    with stylable_container(
-        key="post-home-footer",
-        css_styles="""
-        {
-            font-family: 'Arapey', serif;
-            font-size: 1.1em;
-        }
-        """,
-    ):
-        st.markdown(
-            """**How it works:** Instalicio.us uses cutting edge AI to generate stunning images and
-            posts optimized for engagement and virality based on your prompt.  This could be a description
-            of the dish and a setting, a pasted recipe, a restaurant experience you had, etc.
-            Let your imagination
-            run wild!  If you snap or upload an image, a separate model "looks at" the image,
-            and then factors that in to the post generation as well."""
-        )
-        st.markdown(
-            """**If you need some inspiration**, click the button below to
-            see some examples of posts that Instalicio.us has generated.
-            Whether you are trying to enhance an image you already have or
-            generate a completely new one, we think you will find the tool useful and delightful."""
-        )
-        examples_button = st.button("See Examples", type="primary", use_container_width=True)
-        if examples_button:
-            switch_page("Dalle3Examples")
-            st.rerun()
-        st.markdown(
-            """
-            <p style="text-align: left; color: #000000; font-size:1em; margin-top: 30px; margin-left: 5px;">
-            Before you leave, please <a href="http://www.google.com";
-            color="blue";>fill out a quick survey</a>
-            to tell us about your experience and help us get better!</p>
-            """, unsafe_allow_html=True
-        )
 
-    # need_help_button = st.button("Need Help? (Coming Soon)" , type="primary", disabled=True)
-    # about_button = st.button("About (Coming Soon)", type="primary", disabled=True)
+    st.markdown(
+        """<p style='text-align:center; font-family:"Arapey";'>If you need some inspiration,
+        click the button below to
+        see some examples of posts that Instalicio.us has generated.
+        Whether you are trying to enhance an image you already have or
+        generate a completely new one, we think you will find the tool useful and delightful.</p>""",
+        unsafe_allow_html=True
+    )
+    examples_button = st.button("See Examples", type="primary", use_container_width=True)
+    if examples_button:
+        switch_page("Dalle3Examples")
+        st.rerun()
 
 async def display_post():
     """ Display the post and the image """
@@ -335,14 +316,6 @@ async def display_post():
             display_image(st.session_state.generated_image)
             get_image_download_link(st.session_state.generated_image, filename="instalicious_image.png")
 
-    st.markdown(
-        """
-        <p style="text-align: left; color: #000000; font-size:1em; margin-top: 30px; margin-left: 5px;">
-        Want to Start Over? Before you do, please
-        <a href="http://www.google.com"; color="blue";>fill out a quick survey</a>
-        to tell us about your experience and help us get better!</p>
-        """, unsafe_allow_html=True
-    )
     generate_new_post_button = st.button("Generate New Post", type="primary", use_container_width=True)
     if generate_new_post_button:
         # Reset the session state
