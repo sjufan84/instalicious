434609aeb1f60c57781ec6096593bbf912953cd1
David Thomas <dave_thomas@enoughwebapp.com>
2024-02-01 21:43:03 -0600

    updated tests

--- dalle3_main.py
+++ dalle3_main.py
@@ -4,13 +4,39 @@ import asyncio
 import io
 from PIL import Image
 from pillow_heif import register_heif_opener
-from streamlit_extras.stylable_container import stylable_container
 import streamlit.components.v1 as components
+from streamlit_extras.stylable_container import stylable_container
 from streamlit_extras.switch_page_button import switch_page
+from openai import OpenAIError
+from dependencies import get_openai_client
 from utils.image_utils import generate_dalle3_image
-from utils.post_utils import create_post, alter_image
+from utils.post_utils import alter_image, get_image_prompt
 import logging
 import base64
+""" Testing """
+register_heif_opener()
+
+# Create the OpenAI client
+client = get_openai_client()
+
+def heic_to_base64(heic_path):
+    # Read HEIC file
+    heif_file = Image.open(heic_path)
+
+    # Convert to RGB
+    img = heif_file.convert("RGB")
+
+    # Create an in-memory file object
+    buffer = io.BytesIO()
+
+    # Save the image to the in-memory file object
+    img.save(buffer, format="JPEG")
+
+    # Encode to Base64
+    img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    return img_base64
+
 
 st.set_page_config(
     page_title="Instalicio.us",
@@ -31,36 +57,17 @@ st.markdown(
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-register_heif_opener()
-
-def heic_to_base64(heic_path):
-    # Read HEIC file
-    heif_file = Image.open(heic_path)
-
-    # Convert to RGB
-    img = heif_file.convert("RGB")
-
-    # Create an in-memory file object
-    buffer = io.BytesIO()
-
-    # Save the image to the in-memory file object
-    img.save(buffer, format="JPEG")
-
-    # Encode to Base64
-    img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-
-    return img_base64
-
 # Initialize the session state
 def init_session_variables():
     # Initialize session state variables
     session_vars = [
-        "image_model", "user_image_string", "generate_image", "size_choice",
-        "current_post", "current_hashtags", "current_image_prompt", "post_page", "generated_image"
+        "image_model", "user_image_string", "generate_image", "post_prompt",
+        "current_post", "current_hashtags", "current_image_prompt", "post_page",
+        "generated_image", "size_choice"
     ]
     default_values = [
-        "dall-e-3", None, False, "1024x1024", None, None, None, "post_verify",
-        None
+        "dall-e-3", None, False, None, None, None, None, "post_verify",
+        [], None
     ]
 
     for var, default_value in zip(session_vars, default_values):
@@ -69,7 +76,7 @@ def init_session_variables():
 
 def reset_session_variables():
     session_vars = [
-        "image_model", "is_user_image", "user_image_string", "generate_image", "size_choice",
+        "image_model", "is_user_image", "user_image_string", "generate_image", "post_prompt"
         "current_post", "current_hashtags", "current_image_prompt", "generated_image"
     ]
     for var in session_vars:
@@ -113,7 +120,6 @@ def post_verify():
             st.warning("Incorrect password. Please try again.")
 
 async def post_home():
-    image_string = None
     with stylable_container(
         key="post-home-container",
         css_styles="""
@@ -135,8 +141,6 @@ async def post_home():
             Transform any meal into a stunning,
             Insta-worthy post... instantly.</p>""", unsafe_allow_html=True
         )
-    st.text("")
-
     with stylable_container(
         key="post-main",
         css_styles="""
@@ -158,7 +162,6 @@ async def post_home():
             a restaurant experience you had, etc.\n\n Let your imagination run wild!"""
         )
         st.text("")
-
         picture_mode = st.selectbox(
             '###### 📸 Snap a Pic, 📤 Upload an Image, or Let Us Generate One For You!',
             ("Snap a pic", "Upload an image", "Let Us Generate One For You"), index=None,
@@ -175,7 +178,9 @@ async def post_home():
 
         elif picture_mode == "Upload an image":
             # Show a file upoloader that only accepts image files
-            uploaded_image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg", "heic", "HEIC"])
+            uploaded_image = st.file_uploader(
+                "Upload an image", type=["png", "jpg", "jpeg", "heic", "HEIC"]
+            )
             # Convert the image to a base64 string
             if uploaded_image:
                 # If the file type is .heic or .HEIC, convert to a .png using PIL
@@ -186,44 +191,30 @@ async def post_home():
                     image_string = await encode_image(uploaded_image)
                 st.session_state.user_image_string = image_string
         elif picture_mode == "Let Us Generate One For You":
-            st.session_state.generate_image = True
-
+            st.session_state.user_image_string = None
         st.text("")
         post_prompt = st.text_area("""###### Tell Us About This Recipe or Meal""")
 
-    size_choice = st.radio(
-        "Select your desired format:", options=["Square", "Stories"], horizontal=True, index=None,
-    )
-    if size_choice == "Square":
-        st.session_state.size_choice = "1024x1024"
-    elif size_choice == "Stories":
-        st.session_state.size_choice = "1024x1792"
-    generate_post_button = st.button("Generate Post", type="primary", use_container_width=True)
-    logger.debug(f"Generate post button pressed: {generate_post_button}")
-    if generate_post_button:
-        if picture_mode and post_prompt != "" and size_choice and st.session_state.user_image_string:
-            with st.spinner("Generating your post. This may take a minute..."):
-                image_prompt = await alter_image(post_prompt, st.session_state.user_image_string)
-                st.session_state.current_image_prompt = image_prompt
-                post = await create_post(prompt=post_prompt, post_type="no_image")
-                st.session_state.current_post = post["post"]
-                st.session_state.current_hashtags = post["hashtags"]
+        size_choice = st.radio(
+            "Select your desired format:", options=["Square", "Stories"], horizontal=True, index=None,
+        )
+        if size_choice == "Square":
+            st.session_state.size_choice = "1024x1024"
+        elif size_choice == "Stories":
+            st.session_state.size_choice = "1024x1792"
+
+        generate_post_button = st.button("Generate Post", type="primary", use_container_width=True)
+        logger.debug(f"Generate post button pressed: {generate_post_button}")
+        if generate_post_button:
+            if picture_mode and post_prompt != "" and st.session_state.size_choice is not None:
+                st.session_state.post_prompt = post_prompt
                 st.session_state.post_page = "display_post"
                 st.rerun()
-        elif picture_mode and post_prompt != "" and size_choice and not st.session_state.user_image_string:
-            with st.spinner("Generating your post. This may take a minute..."):
-                post = await create_post(prompt=post_prompt, post_type="with_image")
-                st.session_state.current_post = post["post"]
-                st.session_state.current_hashtags = post["hashtags"]
-                st.session_state.current_image_prompt = post["image_prompt"]
-                st.session_state.post_page = "display_post"
-                st.rerun()
-        else:
-            st.warning("Please make an image choice, enter a description, and select your preferred format.")
+            else:
+                st.warning("Please make an image choice, size choice, and enter a prompt.")
 
     st.text("")
     st.text("")
-
     st.markdown(
         """<p style='text-align:center; font-family:"Arapey";'>If you need some inspiration,
         click the button below to
@@ -234,7 +225,7 @@ async def post_home():
     )
     examples_button = st.button("See Examples", type="primary", use_container_width=True)
     if examples_button:
-        switch_page("Dalle3Examples")
+        switch_page("Dalle3StreamingExamples")
         st.rerun()
 
 async def display_post():
@@ -276,48 +267,86 @@ async def display_post():
         """,
     ):
         logger.debug("Entering display_post function")
-        # Convert the list of hashtags to a string with a space in between and
-        # a "#" in front of each hashtag
-        hashtags_string = " ".join(["#" + hashtag for hashtag in st.session_state["current_hashtags"]])
-        # Use the post-content style to display the post
-        st.markdown(f'''
-            <p style="font-weight: semibold; margin: 15px;">{st.session_state['current_post']}</p>
-            </div>
-        ''', unsafe_allow_html=True)
+        messages = [
+            {
+                "role": "system", "content": f"""You are a helpful assistant helping a user optimize and
+                create posts for Instagram centered around food and cooking.The user would like
+                for you to generate an Instagram post optimized
+                for engagement and virality based on the prompt {st.session_state.post_prompt}
+                they have given.  This could be a recipe, a description of a dish,
+                a description of a restaurant experience, etc.
+                If it is a recipe, you do not need to return the recipe itself,
+                Make sure to that the post includes hashtags
+                and that the post is presented in a clear
+                and organized manner.  Return only the generated post text and hashtags.
+                You do not need to return any other message content other than a note
+                at the end that says something like "Now give us just a sec, and we will generate
+                an amazing image for you to go with your post."
+                """
+            },
+            {
+                "role" : "user",
+                "content" : f"""Can you help me generate an amazing
+                Instagram post based on this prompt {st.session_state.post_prompt}?"""
+            }
+        ]
+
+    message_placeholder = st.empty()
+    full_response = ""
+    if st.session_state.current_post is None:
+        try:
+            completion = client.chat.completions.create(
+                model="gpt-4-turbo-preview",
+                messages=messages,
+                stream=True,
+            )
+            for chunk in completion:
+                if chunk.choices[0].finish_reason == "stop":
+                    logging.debug("Received 'stop' signal from response.")
+                    break
+                full_response += chunk.choices[0].delta.content
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+            st.session_state.current_post = full_response
+        except OpenAIError as e:
+            logger.error(f"Error generating post: {e}")
+            st.error(f"Error generating post: {e}")
+    if st.session_state.current_post:
+        html = f"""
+        <input type="text" id="textToCopy" value="{st.session_state.current_post}" style="color:transparent;
+        border-color:transparent;">
+        <button onclick="copyToClipboard()" style="background-color:transparent; height: 2.75em;
+        margin-left: 7em; border-radius:4px; font-size:1em;">Copy Post 📋</button>
+
+        <script>
+            function copyToClipboard() {{
+                var copyText = document.getElementById("textToCopy");
+                copyText.select();
+                copyText.setSelectionRange(0, 99999); /* For mobile devices */
+                document.execCommand("copy");
+                alert("Copied the text: " + copyText.value);
+            }}
+        </script>
+        """
+        components.html(html, height=75)
         st.text("")
-        st.markdown(f'''
-            <p style="color:#203590; font-weight: bold; margin: 15px 15px 30px 15px;">{hashtags_string}</p>
-            </div>
-        ''', unsafe_allow_html=True)
-
-    total_post = st.session_state.current_post + " " + hashtags_string
-    html = f"""
-    <input type="text" id="textToCopy" value="{total_post}" style="color:transparent;
-    border-color:transparent;">
-    <button onclick="copyToClipboard()" style="background-color:transparent;
-    margin-left: 8.75em; height: 2.75em; border-radius:4px; text-align: center;">Copy Post 📋</button>
-
-    <script>
-        function copyToClipboard() {{
-            var copyText = document.getElementById("textToCopy");
-            copyText.select();
-            copyText.setSelectionRange(0, 99999); /* For mobile devices */
-            document.execCommand("copy");
-            alert("Copied the text: " + copyText.value);
-        }}
-    </script>
-    """
-    components.html(html, height=75)
+    if not st.session_state.generated_image != [] and st.session_state.user_image_string:
+        with st.spinner("Hang tight, we are generating your image..."):
+            image_prompt = await alter_image(st.session_state.post_prompt, st.session_state.user_image_string)
+            st.session_state.generated_image = await generate_dalle3_image(
+                prompt=image_prompt
+            )
+    elif not st.session_state.generated_image != [] and st.session_state.user_image_string is None:
+        with st.spinner("Hang tight, we are generating your image..."):
+            image_prompt = await get_image_prompt(st.session_state.post_prompt)
+            st.session_state.generated_image = await generate_dalle3_image(
+                prompt=image_prompt
+            )
 
     st.markdown(
         """<p style='text-align: center; color: #000000;
         font-size: 20px; font-family:"Arapey";'>Here's your image!</p>""", unsafe_allow_html=True
     )
-    if not st.session_state.generated_image:
-        with st.spinner("Generating your image..."):
-            st.session_state.generated_image = await generate_dalle3_image(
-                st.session_state["current_image_prompt"]
-            )
     if st.session_state.generated_image:
         with stylable_container(
             key="image-display-container",
@@ -337,6 +366,7 @@ async def display_post():
     if generate_new_post_button:
         # Reset the session state
         reset_session_variables()
+        st.session_state.current_post = None
         st.rerun()
 
 if st.session_state.post_page == "post_verify":
